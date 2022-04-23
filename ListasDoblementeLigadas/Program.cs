d42bac24320d4538f030a9e420c78760d135bc8a
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-04-23 14:33:29 -0700

    Creamos la Lista con los metodos

--- ListasDoblementeLigadas/Program.cs
+++ ListasDoblementeLigadas/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListasDoblementeLigadas;
+
+Lista listas = new Lista();
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Tres");
+Console.WriteLine(listas.RecorrerLista());
+
+Console.WriteLine("Buscando Nodo");
+Nodo nodoDos = listas.Buscar("Dos");
+Console.WriteLine(nodoDos.Valor);
