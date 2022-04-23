a3c7ad859e5a054acfc92b9921199b5a2295de92
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-04-23 13:49:18 -0700

    Add project files.

---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,16 @@
+﻿using ListasSimplementeLigadas;
+
+Lista listas = new Lista();
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Ocho");
+Console.WriteLine(listas.RecorrerLista());
+
+Console.WriteLine("Buscando Nodo");
+Nodo nodoDos = listas.Buscar("Dos");
+Console.WriteLine(nodoDos.Valor);
+
+Console.WriteLine("Borrando Dos");
+listas.BorrarNodo("Dos");
+Console.WriteLine(listas.RecorrerLista());
