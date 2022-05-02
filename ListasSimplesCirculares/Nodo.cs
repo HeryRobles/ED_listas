f4d83527d7b14be0b7dacd197fe00e4b8b52c3f2
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-05-02 11:48:00 -0700

    Crear la Clase Nodo para darle atributos y propiedades al Nodo

---
+++ ListasSimplesCirculares/Nodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasSimplesCirculares
+{
+    public class Nodo
+    {
+        public string dato;
+
+    }
+}
