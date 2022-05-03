d38af1247d8e5bcc6d880a52b0c941ea2af54a6f
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-05-02 22:00:26 -0700

    Agg la Clase Nodo de la ListaDoble Circular

---
+++ ListasDoblesCirculares/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblesCirculares
+{
+    public class Nodo
+    {
+        public string Valor { get; set; }   
+        
+        public Nodo EnlacePrimero { get; set; }
+        public Nodo EnlaceUltimo { get; set; }
+
+        public Nodo(string valor = "", Nodo enlacePrimero = null)
+        {
+            Valor = valor;
+            EnlacePrimero = enlacePrimero;
+        }
+    }
+}
